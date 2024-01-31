39ddd12f1cfb94824c3611a23b1d67abf49a7001
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-31 18:06:17 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(item => item.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(item => {
+        const test = item.slice(0, string.length);
+        return test === string;
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(item => item.name === string);
+}
